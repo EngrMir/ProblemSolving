a4e819c4a5f5905e59c7330cc50a48e300d7e164
Mir Jibon <enggjibon.coding@gmail.com>
2017-09-29 17:49:38 +0600

    Itteration Small Problems Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    Itteration Small Problems
    .wq
    
    `#

---
+++ Problem_Itteration/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Itteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            //// **************** Factorial Problem ****************
+            //Console.WriteLine("Please Enter Your Number");
+            //int n = int.Parse(Console.ReadLine());
+            //int factorial = 1;
+            //for (var i = 1; i < n; i++)
+            //{
+            //    factorial *= i;
+            //}
+            //Console.WriteLine(factorial);
+            //Console.Read();
+
+            //// **************** Date Time Problem ****************
+            //DateTime dat = DateTime.Now;
+
+            //Console.WriteLine("\nToday is {0:d} at {0:T}.", dat);
+            //Console.Write("\nPress any key to continue... ");
+            //Console.ReadLine();
+
+            //// **************** Asterstic Problem ****************
+            ////Let’s print a triangle made of asterisks(‘*’) separated by spaces.The triangle should consist of n
+            ////rows, where n is a given positive integer, and consecutive rows should contain 1, 2, . . . , n
+            ////asterisks.For example, for n = 4 the triangle should appear as follows:
+            ////*
+            ////**
+            ////***
+            ////****
+
+            //Console.WriteLine("Please Enter the Number of * ");
+            // int n = int.Parse(Console.ReadLine());
+
+            // for (var i = 1; i <= n; i++)
+            // {
+            //     for (var k = 1; k <= i; k++)
+            //     {
+            //         Console.Write("*");
+
+            //     }
+            //     Console.WriteLine("");
+            // }
+            // Console.ReadLine();
+
+
+            //// Let’s print a triangle made of asterisks(‘*’) separated by spaces and consisting of n
+            //// rows again, but this time upside down, and make it symmetrical. Consecutive rows should
+            //// contain 2n−1,2n−3 , . . . , 3, 1 asterisks and should be indented by 0, 2, 4, . . . ,2(n−1)
+            //// spaces.For example, for n= 4 the triangle should appear as follows:
+            ////        *******
+            ////         *****
+            ////          ***
+            ////           *
+
+            ////The triangle should have n rows, where n is some given positive integer.
+            //Console.WriteLine("PLease Enter the Number of Asterestic");
+            //int number = int.Parse(Console.ReadLine());
+            //int i, j, k;
+
+            //for (i = 1; i <= number; i++)
+            //{
+            //    for (j = 1; j <= number - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine("");
+            //}
+            //for (i = 2 * number - 1; i >= 1; i--)
+            //{
+            //    for (j = 1; j < 2 * number - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    i--;
+            //    Console.WriteLine(" ");
+            //}
+            //Console.ReadLine();
+
+            ////// Calculate Fibonacci Series
+
+
+
+
+        }
+    }
+}
+
+
+
